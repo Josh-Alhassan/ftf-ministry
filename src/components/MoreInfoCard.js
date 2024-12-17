147bbd1926d870c892cb9cb4e0f85615b3171f66
Josh-Alhassan <saintjosh26@gmail.com>
2024-12-17 03:57:18 +0100

    modify card content

--- src/components/MoreInfoCard.js
+++ src/components/MoreInfoCard.js
@@ -15,18 +15,16 @@ function MoreInfoCard() {
         hoverBackgroundColor="#FDF0EC"
       />
       <Card
-        secondaryHeading="Who we are"
-        primaryHeading="Know about us"
-        paragraph="We Exist to Win, Equip and Deploy (WED) supernatural leaders everywhere
-        taking over every sphere."
+        secondaryHeading="Be in the know"
+        primaryHeading="Connect with us"
+        paragraph="Join our mailing list to get amazing update from faith of the faithful ministries and Pst. David Isah."
         backgroundColor="#F5F9FE"
         hoverBackgroundColor="#F5F9FE"
       />
       <Card
-        secondaryHeading="Who we are"
-        primaryHeading="Know about us"
-        paragraph="We Exist to Win, Equip and Deploy (WED) supernatural leaders everywhere
-        taking over every sphere."
+        secondaryHeading="Monday Gathering"
+        primaryHeading="Follow us online"
+        paragraph="We meet every monday by 4:30pm WAT and Wednesdays by 9PM WAT online. Join us this week."
         backgroundColor="#E8EEF1"
         hoverBackgroundColor="#E0E2F0"
       />
